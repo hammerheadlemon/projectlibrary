1c41012bc52a2a6e1173a952f304ebc559d2eafa
Will Grant <will.granty@yahoo.co.uk>
2019-08-22 21:18:45 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="projectlibrary",
+      version="0.1",
+      description="Test Project Description",
+      author="Mr Grant",
+      packages=['analysis']
+      )
