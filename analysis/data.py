4bbaf58085e88cea66b9582fd1304842bb54525d
Will Grant <will.granty@yahoo.co.uk>
2019-09-17 12:04:16 +0100

    changes to master data file paths

--- analysis/data.py
+++ analysis/data.py
@@ -3,7 +3,7 @@ from bcompiler.utils import project_data_from_master
 
 '''place for storing all master templates'''
 
-q1_1920 = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_1_2019_wip_(25_7_19).xlsx')
+q1_1920 = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_1_2019.xlsx')
 q4_1819 = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_4_2018.xlsx')
 q3_1819 = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_3_2018.xlsx')
 q2_1819 = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_2_2018.xlsx')
@@ -14,6 +14,8 @@ q2_1718 = project_data_from_master('C:\\Users\\Standalone\\general\\masters fold
 q1_1718 = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_1_2017.xlsx')
 q4_1617 = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_4_2016.xlsx')
 q3_1617 = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_3_2016.xlsx')
+
+q1_1920_test = project_data_from_master('C:\\Users\\Standalone\\general\\masters folder\\core_data\\master_1_2019_test_one.xlsx')
 
 '''list of dictionaries'''
 one_quarter_dict_list = [q1_1920]
