06af8caaf16dd284e0b541ff57aba9ca698d5be8
me <will.granty@yahoo.co.uk>
2020-01-14 13:19:12 +0000

    adding file paths for windows users.

--- analysis/data.py
+++ analysis/data.py
@@ -17,6 +17,20 @@ q1_1718 = project_data_from_master('/home/will/Documents/portfolio/core_date/mas
 q4_1617 = project_data_from_master('/home/will/Documents/portfolio/core_date/master_4_2016.xlsx', 4, 2016)
 q3_1617 = project_data_from_master('/home/will/Documents/portfolio/core_date/master_3_2016.xlsx', 3, 2016)
 
+#data paths for window users
+# q2_1920 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_2_2019.xlsx', 2, 2019)
+# q1_1920 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_1_2019.xlsx', 1, 2019)
+# q4_1819 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_4_2018.xlsx', 4, 2018)
+# q3_1819 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_3_2018.xlsx', 3, 2018)
+# q2_1819 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_2_2018.xlsx', 2, 2018)
+# q1_1819 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_1_2018.xlsx', 1, 2018)
+# q4_1718 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_4_2017.xlsx', 4, 2017)
+# q3_1718 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_3_2017.xlsx', 3, 2017)
+# q2_1718 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_2_2017.xlsx', 2, 2017)
+# q1_1718 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_1_2017.xlsx', 1, 2017)
+# q4_1617 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_4_2016.xlsx', 4, 2016)
+# q3_1617 = project_data_from_master('C:\\Users\\Standalone\\general\\core_data\\master_3_2016.xlsx', 3, 2016)
+
 """list of dictionaries"""
 one_quarter_master_list = [q1_1920]
 bespoke_group_masters_list = [q2_1819, q1_1819]
