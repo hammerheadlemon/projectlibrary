5b26f665ec6513a23ae4b45ea3bc0752846456d5
me <will.granty@yahoo.co.uk>
2020-01-21 08:44:26 +0000

    changes to improve handling of baselines

--- analysis/engine_functions.py
+++ analysis/engine_functions.py
@@ -12,7 +12,10 @@ import datetime
 from openpyxl.styles import Font, PatternFill
 from openpyxl.styles.differential import DifferentialStyle
 from openpyxl.formatting import Rule
-from analysis.data import q3_1920, q2_1920
+from analysis.data import q2_1920
+from datamaps.api import project_data_from_master
+
+last_qrt = project_data_from_master('/home/will/Documents/portfolio/core_date/master_2_2019.xlsx', 2, 2019)
 
 '''dates for functions. python date format is Year, Month, day'''
 bicc_date = datetime.date(2019, 2, 10)
@@ -503,7 +506,7 @@ def baseline_index(baseline_data):
     output = {}
 
     for project_name in baseline_data:
-        if project_name in q2_1920:
+        if project_name in last_qrt.projects:
             lower_list = [0, 1]
             for tuple_info in baseline_data[project_name]:
                 lower_list.append(tuple_info[2])
